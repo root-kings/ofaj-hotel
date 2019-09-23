c9aa52916b26de596dddb082e94bd21c6733d557
Pratik-sys <pithore.pratik@gmial.com>
2019-09-23 21:10:38 +0530

    repush the files

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home center">
+    <camera />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Camera from "@/components/Camera.vue";
+
+export default {
+  name: "home",
+  components: {
+    Camera
+  }
+};
+</script>
