0213fd841cee22021c610a74ed020e727dc10417
krushndayshmookh <krushndayshmookh@gmail.com>
2019-09-24 11:32:38 +0530

    table fixed

---
+++ src/components/TableO.vue
@@ -0,0 +1,14 @@
+<template>
+	<table>
+		<tr v-for="guest in guests" :key="guest.room">
+			<td>{{ guest.name }}</td>
+			<td>{{ guest.room }}</td>
+		</tr>
+	</table>
+</template>
+
+<script>
+export default {
+	props: ['guests']
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,49 @@
 <template>
-  <div class="home center">
-    <camera />
-  </div>
+	<div class="home center">
+		<camera />
+		<table-o :guests="guests" />
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Camera from "@/components/Camera.vue";
+import TableO from "@/components/TableO.vue";
 
 export default {
-  name: "home",
-  components: {
-    Camera
-  }
+	name: "home",
+	components: {
+		Camera,
+		TableO
+	},
+	data() {
+		return {
+			guests: []
+		};
+	},
+	methods:{
+		fetchGusts(){
+
+			let temp =[
+				{
+					name: "krushn",
+					room: 23
+				},
+				{
+					name: "tushar",
+					room: 24
+				},
+				{
+					name: "adrak",
+					room: 25
+				}
+			]
+
+			this.guests = temp
+		}
+	},
+	mounted(){
+		this.fetchGusts()
+	}
 };
 </script>
